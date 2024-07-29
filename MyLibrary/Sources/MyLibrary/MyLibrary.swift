efd842e4ef9285010f23e26ce74f0d62c3f32774
Carolina Calderon <carolina.calderon@bayteq.com>
2024-07-29 18:37:29 -0500

    update

--- MyLibrary/Sources/MyLibrary/MyLibrary.swift
+++ MyLibrary/Sources/MyLibrary/MyLibrary.swift
@@ -1,2 +1,25 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import SwiftUI
+public struct SimpleView: View {
+    public init() {}
+
+    @available(macOS 10.15, *)
+    public var body: some View {
+        VStack {
+            Text("Hello, Swift Package!")
+                .font(.largeTitle)
+                .padding()
+            Button(action: {
+                print("Button tapped!")
+            }) {
+                Text("Tap me")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+        }
+        .padding()
+    }
+}
